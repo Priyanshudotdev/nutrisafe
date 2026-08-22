e46c0eb051f2024c6ca77e4a7f0e0ebe9947f959
Priyanshudotdev <priyanshu.kayarkar009@gmail.com>
2026-08-22 23:25:43 +0530

    feat: new beginning

--- convex/http.ts
+++
@@ -1,9 +0,0 @@
-import { httpRouter } from "convex/server";
-import { authComponent, createAuth } from "./auth/auth";
-
-const http = httpRouter();
-
-// Register all Better Auth routes (handles /api/auth/* automatically)
-authComponent.registerRoutes(http, createAuth);
-
-export default http;
